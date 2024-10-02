042e80872d6c8aa4834436ae6cc2c6d0b5f660d4
svitlana1dev <ponomarchuk.svitlana.dev@gmail.com>
2024-10-02 12:16:36 +0300

    Updates

--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var jsx_runtime_1 = require("react/jsx-runtime");
-var client_1 = require("react-dom/client");
-var App_1 = __importDefault(require("./App"));
-(0, client_1.createRoot)(document.getElementById("root")).render((0, jsx_runtime_1.jsx)(App_1.default, {}));
